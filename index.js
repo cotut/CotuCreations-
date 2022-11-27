0d44fe5b6689565233f8f35810ca6ae9c548d8c8
cotu embed <113551611+cotut@users.noreply.github.com>
2022-11-27 08:10:37 +0000

    Added info command

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 
 
 
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS", "DIRECT_MESSAGES", "GUILD_INTEGRATIONS"] });
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS", "DIRECT_MESSAGES", "GUILD_INTEGRATIONS", "GUILD_PRESENCES"] });
 
 client.login(process.env.BOT);
 
@@ -76,5 +76,111 @@ client.on('interactionCreate', inte => {
 
 	if(inte.commandName === 'ping'){
 		inte.reply({content: `**Your latency is of *${client.ws.ping}* ms**`, ephemeral: true})
+	} else if(inte.commandName === 'info'){
+		const user = inte.options.getUser('user')
+		const mem = inte.options.getMember('user')
+
+		if(!user){
+			const info1 = new Discord.MessageEmbed()
+				.setColor('#303136')
+				.setTitle('Info Service')
+				.setFooter('Requested by urself')
+				.setTimestamp()
+				.setThumbnail(inte.user.avatarURL())
+				.addFields(
+					[
+						{
+							name: 'Ping:',
+							value: `<@${inte.user.id}>`,
+							inline: true,
+						},
+						{
+							name: 'Tag:',
+							value: inte.user.tag,
+							inline: true,
+						},
+						{
+							name: 'Id:',
+							value: inte.user.id,
+							inline: true,
+						},
+            {
+							name: 'Status:',
+							value: inte.member.presence?.status || 'offline',
+							inlime: true,
+						},
+						{
+							name: 'Avatar URL:',
+							value: `[Image](${inte.user.avatarURL()})`,
+							inline: true
+						},
+						{
+							name: 'N° Roles:',
+							value: (inte.member.roles.cache.size).toString(),
+							inline: true,
+						},
+						{
+							name: 'Roles',
+							value: inte.member.roles.cache.map(role => `<@&${role.id}>`).join(' , '),
+              inline: true,
+							}
+					]
+				)
+
+			inte.reply({embeds: [info1]})
+		} else {
+
+			
+			const status = mem.presence?.status || 'offline'
+				
+			const info2 = new Discord.MessageEmbed()
+				.setColor('#303136')
+				.setTitle('Info Service')
+				.setThumbnail(user.avatarURL())
+				.setFooter(`Requested by ${inte.user.tag}`)
+				.setTimestamp()
+				.addFields(
+					[
+						{
+							name: 'Ping:',
+							value: `<@${user.id}>`,
+							inline: true,
+						},
+						{
+							name: 'Tag:',
+							value: user.tag,
+							inline: true,
+						},
+						{
+							name: 'Id:',
+							value: user.id,
+							inline: true,
+						},
+						{
+							name: 'Status:',
+							value: status,
+							inline: true,
+						},
+						{
+							name: 'Avatar URL:',
+							value: `[Image](${user.avatarURL()})`,
+							inline: true
+						},
+						{
+							name: 'N° Roles:',
+							value: (mem.roles.cache.size).toString(),
+							inline: true
+						},
+						{
+							name: 'Roles',
+							value: mem.roles.cache.map(role => `<@&${role.id}>`).join(' , '),
+              inline: true,
+						}
+					]
+
+				)
+
+			inte.reply({embeds: [info2]})
+		}
 	}
 })
